64f3fc8dc5f54b2d9fd6cdf196c48039c4451542
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-09-23 10:29:02 +0300

    feat: Moved the db connection to new file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3002;
 const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT;
 //Using CORS
 app.use(cors());
 app.use(express.json());
